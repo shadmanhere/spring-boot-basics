da33fb7da77501f9b0ffe03ec84d6c1ed428f253
Shadman Ali <shadman.ali@live.co.uk>
2022-06-16 05:17:18 +0530

    /employee rest endpoint created

---
+++ src/main/java/com/example/demo/controller/EmployeeController.java
@@ -0,0 +1,17 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class EmployeeController {
+	// localhost:8080/employees
+	
+	@RequestMapping(value = "/employees", method=RequestMethod.GET)
+	@ResponseBody
+	public String getEmployees() {
+		return "displaying the list of employees";
+	}
+}
