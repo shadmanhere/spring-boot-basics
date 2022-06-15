c01c1f6e93f7f3310c6ef65c48f93a9bfa75e7a1
Shadman Ali <shadman.ali@live.co.uk>
2022-06-16 05:22:32 +0530

    switched to @RestController from @Controller + @ResponseBody annotations

--- src/main/java/com/example/demo/controller/EmployeeController.java
+++ src/main/java/com/example/demo/controller/EmployeeController.java
@@ -4,13 +4,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+//@Controller
+@RestController //@Controller + @ResponseBody
 public class EmployeeController {
 	// localhost:8080/employees
 	
 	@RequestMapping(value = "/employees", method=RequestMethod.GET)
-	@ResponseBody
+//	@ResponseBody
 	public String getEmployees() {
 		return "displaying the list of employees";
 	}
