68e95412b6528994dc095213c8772243332f3a4c
Shadman Ali <shadman.ali@live.co.uk>
2022-06-17 11:58:07 +0530

    lombook and spring devtool added

--- src/main/java/com/example/demo/controller/EmployeeController.java
+++ src/main/java/com/example/demo/controller/EmployeeController.java
@@ -1,21 +1,19 @@
 package com.example.demo.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-//@Controller
 @RestController //@Controller + @ResponseBody
 public class EmployeeController {
-	// localhost:8080/employees
 	
-//	@RequestMapping(value = "/employees", method=RequestMethod.GET)
-//	@ResponseBody
 	@GetMapping("/employees")
 	public String getEmployees() {
 		return "displaying the list of employees";
+	}
+	
+	@GetMapping("/employees/{id}")
+	public String getEmployee(@PathVariable("id") Long id){
+		return "employee id is "+id;
 	}
 }
