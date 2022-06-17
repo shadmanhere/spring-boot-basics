69260bb0e87ad046fcc27e4736d47c6660f96909
Shadman Ali <shadman.ali@live.co.uk>
2022-06-17 12:18:15 +0530

    path varibale and request param used

--- src/main/java/com/example/demo/controller/EmployeeController.java
+++ src/main/java/com/example/demo/controller/EmployeeController.java
@@ -1,7 +1,9 @@
 package com.example.demo.controller;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController //@Controller + @ResponseBody
@@ -15,5 +17,10 @@ public class EmployeeController {
 	@GetMapping("/employees/{id}")
 	public String getEmployee(@PathVariable("id") Long id){
 		return "employee id is "+id;
+	}
+	
+	@DeleteMapping("/employees")
+	public String deleteEmployee(@RequestParam("id") Long id) {
+		return "employee with id "+id+" deleted";
 	}
 }
