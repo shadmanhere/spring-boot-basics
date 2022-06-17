6abf5ce599d905df6e45098718b52f5bb861fe67
Shadman Ali <shadman.ali@live.co.uk>
2022-06-17 12:55:18 +0530

    employee model added

--- src/main/java/com/example/demo/controller/EmployeeController.java
+++ src/main/java/com/example/demo/controller/EmployeeController.java
@@ -3,8 +3,12 @@ package com.example.demo.controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.model.Employee;
 
 @RestController //@Controller + @ResponseBody
 public class EmployeeController {
@@ -17,6 +21,11 @@ public class EmployeeController {
 	@GetMapping("/employees/{id}")
 	public String getEmployee(@PathVariable("id") Long id){
 		return "employee id is "+id;
+	}
+	
+	@PostMapping("/employees")
+	public String saveEmployee(@RequestBody Employee employee) {
+		return "saving the employee details to the database "+employee;
 	}
 	
 	@DeleteMapping("/employees")
